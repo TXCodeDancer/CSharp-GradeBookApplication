d771cfb61934266a8cf31faefc9f601175c525c9
TXCodeDancer <RickBrunson@sbcglobal.net>
2018-09-20 17:18:19 -0500

    Completed the module
    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -49,7 +49,7 @@ namespace GradeBook.UserInterfaces
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a supported type of gradebook, please try again", name);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type.ToString());
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
